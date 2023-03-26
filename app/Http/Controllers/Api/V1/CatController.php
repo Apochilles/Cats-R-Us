dcabb831b52393b17d654f12c3e65a1d7fe4e545
Luke <l.j.dwsn@gmail.com>
2023-03-26 12:31:02 +1100

    initial commit

---
+++ app/Http/Controllers/Api/V1/CatController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCatRequest;
+use App\Http\Resources\V1\CatResource;
+use App\Models\Cat;
+use Illuminate\Http\Request;
+
+class CatController extends Controller
+{
+    public function index()
+    {
+        return CatResource::collection(Cat::all());
+    }
+
+    public function show(Cat $cat)
+    {
+
+        return new CatResource($cat);
+    }
+
+
+    public function store(StoreCatRequest $request)
+    {
+
+        Cat::create($request->validated());
+        return response()->json("Cat Created");
+    }
+
+    public function update(StoreCatRequest $request, Cat $cat)
+    {
+
+        $cat->update($request->validated());
+        return response()->json("Cat Updated");
+    }
+
+    public function delete(StoreCatRequest $request, Cat $cat)
+    {
+
+        $cat->delete($request->validated());
+        return response()->json("Cat Deleted");
+    }
+}
---
+++ app/Http/Requests/StoreCatRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+
+class StoreCatRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'min:3', 'max:20'],
+            // 'slug' => ['required', 'unique:cats,slug' . $this->cat->id],
+            'slug' => ['required', Rule::unique('cats')->ignore($this->cat)],
+            'fivstatus' => ['required', 'in:positive,negative'],
+            'gender' => ['required', 'in:male,female'],
+            'description' => ['required', 'min:10', 'max:150'],
+            'colour' => ['required', 'in:grey,orange,black'],
+            'temperment' => ['required', 'in:quiet,medium,big'],
+            'size' => ['required', 'in:small,medium,large'],
+
+        ];
+    }
+};
